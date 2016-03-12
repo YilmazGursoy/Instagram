51e57a31002a8da657f79bef79d3c56296fc1927
Yilmaz Gursoy <yilmazgursoyy@gmail.com>
2016-03-12 21:02:49 +0200

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Yilmaz  on 12/03/16.
+//  Copyright © 2016 Yilmaz . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Yilmaz  on 12/03/16.
+//  Copyright © 2016 Yilmaz . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
